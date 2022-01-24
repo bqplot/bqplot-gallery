d9d2db059d9ded212f8c7bf2ee8dc75b4731d368
martinRenou <martin.renou@gmail.com>
2022-01-24 09:48:42 +0100

    Spawn one binder server for all the examples

--- js/index.js
+++ js/index.js
@@ -1,34 +1,29 @@
 $(document).ready(function() {
-  var BUILD_ENDPOINT = BINDERHUB_HOST + '/build/gh';
+  var binderUrl = null;
+  var token = null;
+
+  var buildUrl = BINDERHUB_HOST + '/build/gh/bqplot/bqplot-gallery/main';
+  var evtSource = new EventSource(buildUrl);
+  evtSource.onmessage = function(event) {
+    var data = JSON.parse(event.data);
+    $('#loader_text').html(data.phase);
+    console.log(data.message);
+    if (data.phase === 'ready') {
+      evtSource.close();
+      binderUrl = data.url;
+      token = data.token;
+
+      $('.launch-item.disabled').removeClass("disabled");
+      $('.spinner-container').remove();
+    }
+  };
 
   $('.launch-item').click(function(event) {
-    var name = $(event.currentTarget).data('example-name');
-    var url = $(event.currentTarget).data('example-url');
-    var ref = 'main';
+    if (binderUrl !== null) {
+      var url = $(event.currentTarget).data('example-url');
 
-    $('#loading_modal').modal({
-      backdrop: 'static',
-      keyboard: false,
-      show: true
-    });
-
-    $('#loader_text').html('Launching ' + name);
-
-    var buildUrl = BUILD_ENDPOINT + '/bqplot/bqplot-gallery/' + ref + '?urlpath=voila/render/' + url;
-    var evtSource = new EventSource(buildUrl);
-    evtSource.onmessage = function(event) {
-      var data = JSON.parse(event.data);
-      $('#loader_text').html(data.phase);
-      console.log(data.message);
-      if (data.phase === 'ready') {
-        evtSource.close();
-        var redirectUrl = data.url;
-        var token = data.token;
-        var redirect = redirectUrl + 'voila/render/' + url + '?token=' + token;
-        $('#loader_text').html('Launching');
-        window.location.href = redirect;
-      }
-    };
-    return false;
+      var voilaUrl = binderUrl + 'voila/render/' + url + '?token=' + token;
+      window.open(voilaUrl, '_blank').focus();
+    }
   });
 });
